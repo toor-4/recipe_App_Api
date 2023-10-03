08a2a6e55634bfd4fd1e587d129dcb444a5a396f
Alfredo <alfred@frealchar.com/bin/python3>
2023-10-04 00:02:19 +0800

    added Ingredients

--- recipeApi/recipeApi/tests.py
+++ recipeApi/recipeApi/tests.py
@@ -3,7 +3,7 @@ Sample tests
 """
 from django.test import SimpleTestCase
 
-from app import calc
+from . import calc
 
 
 class CalcTests(SimpleTestCase):
--- recipeApi/recipeApi/urls.py
+++ recipeApi/recipeApi/urls.py
@@ -17,7 +17,8 @@ urlpatterns = [
         name='api-docs',
     ),
     path('api/user/', include('user.urls')),
-    # path('api/recipe/', include('recipe.urls')),
+    path('api/recipe/', include('recipe.urls')),
+    # path('api/ingredient/', include('ingredient.urls')),
 ]
 
 # if settings.DEBUG:
